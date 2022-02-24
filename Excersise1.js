88e783e1c3af5afe1a0f968fd40dacde251a222e
ArenTigranyan <99825558+ArenTigranyan@users.noreply.github.com>
2022-02-24 20:11:29 +0400

    Add files via upload

---
+++ Excersise1.js
@@ -0,0 +1,8 @@
+let a = +prompt("Input your first number")
+let b = +prompt("Input your second number")
+let sum = a + b
+let difference = a - b
+let product = a * b
+let quotient = a / b
+let remainder = a % b
+console.log('Your results are ' + sum + ','+ difference + ','+ product + ','+ quotient + ','+ remainder)

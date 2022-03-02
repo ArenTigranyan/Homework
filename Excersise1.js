92766dc896bd8ef8205292ac8002a47becef9a8a
ArenTigranyan <99825558+ArenTigranyan@users.noreply.github.com>
2022-03-02 18:40:37 +0400

    Delete Excersise1.js

--- Excersise1.js
+++
@@ -1,8 +0,0 @@
-let a = +prompt("Input your first number")
-let b = +prompt("Input your second number")
-let sum = a + b
-let difference = a - b
-let product = a * b
-let quotient = a / b
-let remainder = a % b
-console.log('Your results are ' + sum + ','+ difference + ','+ product + ','+ quotient + ','+ remainder)
